a13cb40c8704606ea263a13084dc3d45bb427f64
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2023-02-25 03:29:11 -0500

    Parameters on everything, reworked validation, JSON support

--- src/silogue.nim
+++ src/silogue.nim
@@ -1,10 +1,15 @@
 import prologue
+export prologue
 import macros
 import std/strtabs
 import prologue/middlewares/staticfile
 import prologue/middlewares/staticfilevirtualpath
-export prologue
 import strutils
+export strutils
+import std/options
+export options
+import std/json
+export json
 
 # I tried a template, buy I couldn't get that working
 macro strToIdent(s: NimNode): untyped =
@@ -30,93 +35,226 @@ proc genApp*(): Prologue =
       app.use(staticFileMiddleware(staticDir))
   return app
 
+template resptext*(p: untyped): untyped =
+  resp plainTextResponse(p)
+
+template respjson*(p: untyped): untyped =
+  resp jsonResponse(p)
+
 proc runApp*() =
   app.run() 
-
-proc onget*(path: string, body: proc) =
-  app.get(`path`, `body`)
-
-proc onpost*(path: string, body: proc) =
-  app.post(`path`, `body`)
 
 template genBoilerplate*(funcbody: untyped): untyped =
   (proc (ctx: Context) {.async.} = 
     let ctx {.inject.} = ctx
     funcbody)
 
-proc parseParams(params: string): StringTableRef = 
-  # I feel like there's a more effecient way than using a table
-  result = newStringTable()
-  # Also, the keys don't retain their order
-  if params == "" or params == "()":
-    # Nim is being weird and it gives me one or the other
-    return result
-  for s in params.strip(chars={'"'}).split("&"):
-    let kv = s.split(":")
-    result[kv[0]] = kv[1]
-    # There's probably a cleaner way
-
-
-
-#macro optionalGuard(isOptional: untyped) =  
-  #if not boolVal(isOptional):
-  #  let guard = quote do:
-  #    if `pnameFixed` == "":
-  #      await ctx.respond(Http400, "Invalid Format: " & `pname`)
-  #  result.add(guard)
-
-template paramError*(pname: string): untyped =
+template paramError(pname: string): untyped =
   await ctx.respond(Http400, "Invalid Format for Parameter " & pname)
 
-macro ParseQueryString*(p, isOptional: untyped): untyped =
-  # p is actually a strLiteral, but Nim's annoying me when I do that
+# This is for something than directly needs the string from ctx.getParams
+template validate*(valtype: typedesc, body: untyped): untyped =
+ func validateParam*(t: typedesc[valtype], rawval: string): Option[valtype] =
+    let rawval {.inject.} = rawval
+    body
+
+# This is for a type built on top of an existing type
+template validateFrom*(valtype: typedesc, reqtype: typedesc, body: untyped): untyped =
+  func validateParam*(t: typedesc[valtype], rawval: string): Option[valtype] =
+    let rawval {.inject.} = validateParam(reqtype, rawval)
+    body
+
+# Prologue has getQueryParamsOption, and for the other HTTP Verbs too,
+# But I didn't know about that when making this
+# Plus it's not that much extra overhead
+validate(string):
+  if rawval.len != 0:
+    some rawval
+  else:
+    none string
+
+validateFrom(int, string):
+  if rawval.isSome:
+    try:
+      return some(rawval.get().parseInt)
+    except:
+      return none int
+  none int
+
+macro parseQueryParam*(p, k, isOptional: untyped): untyped =
+  let ptype = strToIdent k
   let pident = strToIdent p
   result = nnkStmtList.newTree()
-  let core = quote do:
-    var `pident` = ctx.getQueryParams(`p`)
-  result.add core
-  if not boolVal(isOptional):
-    let check = quote do:
-      if `pident`.len == 0:
+  if not isOptional.boolVal:
+    # This is the identity Node of the tmp holder
+    # It's named oddly so that so that the user doesn't accidentally use it
+    let tmp = ident("TMP" & pident.repr & "TMP")
+    let tmpbod = quote do:
+      let `tmp` = validateParam(typedesc[`ptype`], ctx.getQueryParams(`p`))
+      if `tmp`.isNone:
         paramError(`p`)
-    result.add check
-
-macro ParseQueryInt*(p, isOptional: untyped): untyped =
-  # actually: string, bool
-  let pident = strToIdent p
-  if not boolVal(isOptional):
-    result = quote do:
-      if ctx.getQueryParams(`p`).len == 0:
-        paramError(`p`)
-      var `pident` = parseInt(ctx.getQueryParams(`p`)) 
+      var `pident` = `tmp`.get()
+    result.add tmpbod
   else:
-    result = quote do:
-      var `pident`: Option[int]
-      try:
-        `pident` = some(ctx.getQueryParams(`p`))
-      except:
-        `pident` = none(int)
-      if `pident`.isNone:
-        paramError(`p`)
-      
+    let tmpbod = quote do:
+      var `pident` = validateParam(typedesc[`ptype`], ctx.getQueryParams(`p`))
+    result.add tmpbod
 
-macro proxySetup*(verb, path, params, handler: untyped): untyped =
-  let handleFunc = ident("on" & verb.repr)
-  let parsedParams = parseParams(params.repr)
-  var corebod = nnkStmtList.newTree()
-  var requiredParams: seq[string] = @[]
-  for k, v in parsedParams.pairs:
-    let isOptional = v.contains("?")  # For optional
-    # leading false 
-    let handlerCode = newIdentNode("ParseQuery" & v)
+# These 2 functions are almost the same
+# It would be beneficial to merge them somehow
+macro parsePathParam*(p, k, isOptional: untyped): untyped =
+  let ptype = strToIdent k
+  let pident = strToIdent p
+  result = nnkStmtList.newTree()
+  if not isOptional.boolVal:
+    let tmp = ident("TMP" & pident.repr & "TMP")
+    let tmpbod = quote do:
+      let `tmp` = validateParam(typedesc[`ptype`], ctx.getPathParams(`p`))
+      if `tmp`.isNone:
+        paramError(`p`)
+      var `pident` = `tmp`.get()
+    result.add tmpbod
+  else:
+    let tmpbod = quote do:
+      var `pident` = validateParam(typedesc[`ptype`], ctx.getPathParams(`p`))
+    result.add tmpbod
+
+macro parseBodyParam*(p, k, isOptional, format: untyped): untyped =
+  let ptype = strToIdent k
+  let pident = strToIdent p
+  result = nnkStmtList.newTree()
+  if format.strVal == "form": # I should clean this, but it works and I don't feel like it
+    if not isOptional.boolVal:
+      let tmp = ident("TMP" & pident.repr & "TMP")
+      let tmpbod = quote do:
+        let `tmp` = validateParam(typedesc[`ptype`], ctx.getPostParams(`p`))
+        if `tmp`.isNone:
+          paramError(`p`)
+        var `pident` = `tmp`.get()
+      result.add tmpbod
+    else:
+      let tmpbod = quote do:
+        var `pident` = validateParam(typedesc[`ptype`], ctx.getPostParams(`p`))
+      result.add tmpbod
+  elif format.strVal == "json":
+    if k.strVal == "NOSCHEMA":
+      let tmpbod = quote do:
+        var `pident` = parseJson(ctx.request.body())
+      result.add tmpbod
+    else: 
+      let tmpbod = quote do:
+        var `pident` = to(parseJson(ctx.request.body()), `ptype`)
+      result.add tmpbod
+
+macro parseQueryParams*(p: untyped): untyped =
+  let params = p.repr
+  if params == "" or params == "()":
+    # Nim is being weird and it gives me one or the other
+    return
+  result = nnkStmtList.newTree()
+  for s in params.strip(chars={'"'}).split("&"):
+    let kv = s.split(":")
+    if kv.len != 2:
+      echo "Here ", p.repr
+      echo "[ERROR::Silogue] Check your syntax [in param parser]"
+      quit 1
+    let param = kv[0]
+    let ptype = kv[1].replace("?")  # Gets rid of all question marks
+    let isOptional = kv[1].contains("?")
     let part = quote do:
-      `handlerCode`(`k`, `isOptional`)
-    corebod.add part
-  corebod.add handler # We add the handler after we add the checker functions
+      parseQueryParam(`param`, `ptype`, `isOptional`)
+    result.add part
+
+
+# Stupid Nim won't let me return a tuple
+macro parsePathParams*(p: untyped): untyped =
+  let params = p.repr
+  result = nnkStmtList.newTree()
+  if params == "/":
+    return result
+  for s in params.strip(chars={'"'}).split("/"):
+    let news = s.strip(chars={'{', '}'})
+    if s != news:
+      # Will be false if name == name
+      # But not when name == {name:string}
+      let kv = news.split(":")
+      # This code feels redundant,
+      # But I'll need to learn more Nim first
+      # to make it better
+      if kv.len != 2:
+        echo "HeRe ", p.repr
+        echo "Some sort of syntax error probably"
+        quit 1
+      let param = kv[0]
+      let ptype = kv[1].replace("?")
+      let isOptional = kv[1].contains("?")
+      let part = quote do:
+        parsePathParam(`param`, `ptype`, `isOptional`)
+      result.add part
+  return result
+
+macro parseBodyParams*(p: untyped): untyped =
+  if p.repr == "()" or p.repr == "":
+    return
+  let format = p[0].strVal.strip(chars={'"'})
+  let params = p[1].strVal
+  result = nnkStmtList.newTree()
+  for s in params.split('&'):
+    let kv = s.split(":")
+    if kv.len == 1:
+      let param = kv[0]
+      let isOptional = param.contains("?")
+      let part = quote do:
+        parseBodyParam(`param`, "NOSCHEMA", `isOptional`, `format`)
+      result.add part
+    elif kv.len != 2:
+      echo "Somethign wrong: ", p.repr
+      echo "Silogue Error, Check you Syntax, probably"
+      quit 1
+    else:
+      let param = kv[0]
+      let ptype = kv[1].replace("?")
+      let isOptional = kv[1].contains("?")
+      let part = quote do:
+        parseBodyParam(`param`, `ptype`, `isOptional`, `format`)
+      result.add part
+
+# I would've merged this in the parsePathParams macro,
+# But stupid Nim won't let me return a tuple
+# if one of the elements is a NimNode
+# TODO: FIX IT
+func fixPath(macpath: string): string =
+  result = "/"
+  for s in macpath.split("/"):
+    if s.len != 0:
+      if not s.contains("{"):
+        result &= s
+      else:
+        result &= s[0 ..< s.find(':')] & "}"
+      result &= '/'
+
+macro proxySetup*(verb, path, params, bodyParams, handler: untyped): untyped =
+  var corebod = nnkStmtList.newTree()
+  
+  let pathBod = quote do:
+    parsePathParams(`path`)
+  let newpath = fixPath(path.repr.strip(chars={'"'}))  # Don't know why I'm doing this
+  corebod.add pathBod
+  
+  let paramCheckBod = quote do:
+    parseQueryParams(`params`)
+  corebod.add paramCheckBod # We add the handler after we add the checker functions
+
+  let bodyCheckBod = quote do:
+    parseBodyParams(`bodyParams`)
+  corebod.add bodyCheckBod
+  
   # Lastly, we wrap it around with the boilerplate function declaration
+  corebod.add handler
+  
   let fbod = newCall("genBoilerplate", corebod)
-  return quote do:
-    `handleFunc`(`path`, `fbod`)
+  result = quote do:
+    app.`verb`(`newpath`, `fbod`)
 
 macro routes*(rs: varargs[untyped]): untyped =
   result = nnkStmtList.newTree()
@@ -132,14 +270,29 @@ macro routes*(rs: varargs[untyped]): untyped =
       let verb = node[0]
       var handler: NimNode
       var params = NimNode()
+      var bodyParams = NimNode()
       if node[1].kind == nnkStrLit:
+        # get "query:params"
         params = node[1]
         handler = node[2]
+      elif node[1].kind == nnkCommand:        
+        # post "query:Params" fmt"Body:Params" 
+        params = node[1][0]
+        bodyParams = node[1][1]
+        handler = node[2]
       elif node[1].kind == nnkStmtList:
+        # get: # No Query Params
         handler = node[1]
+      elif node[1].kind == nnkCallStrLit:
+        # post with"body:params"
+        bodyParams = node[1]
+        handler = node[2]
       else:
+        # Maybe this should be static
+        echo rs.repr
+        echo rs.treeRepr
         # TODO: Make this use Prologue logger
-        echo "Check your Syntax"
+        echo "[ERROR::Silogue] Check your Syntax"
         quit 1
-      result.add newCall(ident "proxySetup", verb, route, params, handler)
+      result.add newCall(ident "proxySetup", verb, route, params, bodyParams, handler)
   result.add newCall("runApp")  # lastly, run app
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,20 +1,51 @@
 import unittest
 import silogue
 
+type User = object
+  name: string
+  age: int
+
 var app = genApp()
 
 routes:
   "/":
-    get "name:string":
-      resp "Hello " & name    
-    post:
-      resp "Posted"
-  "/hello":
+    get "name:string":  # This is a query parameter
+      # By default, resp responds in HTML
+      resp "<h1>Hello " & name & "</h1>"
+  "/optional":
+    # ? Mark for optional values
+    # It returns an Option
+    # Read using val.get(defaultVal)
+    # isSome and isNone check if something was given
+    get "name:string?":
+      if name.isSome:
+        resp "<h1>Hello " & name.get() & "</h1>"
+      else:
+        resp "<h1>Hello anonymous!</h1>"
+  "/json-no-schema":
     get:
-      resp "Lolol"
-  "/eat":
-    get "food:string&times:int":
+      # Send a fake user back
+      let newUser = User(name: "SmortBoi", age: 2)
+      # % returns a JSONNode,
+      # respjson responds to the client with JSON      
+      respjson %newUser
+    post "time:string" json"user":
+      var res = "You really shouldn't use JSON without a schema,"
+      res &= "but I incorporated this feature incase it was needed."
+      res &= "Your name is "
+      if user.hasKey("name"):
+        res &= user["name"].getStr() & "!\n"
+        res &= "Don't give out your name to random websites!!"
+      else:
+        res &= "anonymous.\n"
+        res &= "That's a good thing to do on the web"
+      resptext res
+      # We can tell resp to respond with plaintext instead of HTML
+  "/json-with-schema/{times:int}":
+    # Use {} for path parameters
+    post json"user:User":
+      # POST data in JSON with User Schema
       var res = ""
-      for i in 0..times:
-        res &= "You would like to eat " & food & "\n"
-      resp plainTextResponse(res)
+      for i in 1..times:
+        res &= user.name & " is " & $user.age & " years old!\n"
+      resptext res
